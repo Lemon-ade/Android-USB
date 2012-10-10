c8198ec0fdeb011a4e4ea9732246dc0bb75096e2
In-gu Kang <2002gig@naver.com>
2012-10-10 20:48:51 +0900

    remove files

--- Apps/LTouchpad/gen/app/android/ltouchpad/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package app.android.ltouchpad;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Apps/katalkPCLinker_android/gen/app/android/server/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package app.android.server;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
